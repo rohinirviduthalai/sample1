ba197fad70e3e55822aaf258c8173dbf5cbe2c5e
Rohini <rohini@gmail.com>
2024-04-09 10:35:41 -0500

    added project 1 python script

---
+++ project1/Kodekloud_excerise.py
@@ -0,0 +1,28 @@
+#Pgm1:
+#for letter in 'KodeKloud':
+   # if letter == 'e':
+    #    continue
+    #print('Letter : ' + letter)
+    
+   
+#Pgm2:
+sum = 0
+values = [2,9,1,7]
+for number in values:
+    sum += number
+print(sum)
+    
+#pgm3:
+for x in [0, 1, 1, 3]:
+    for y in [0, 2, 1, 2]:
+            print('*')
+            
+#pgm 4:
+year = 1905
+year = year // 100
+#if(year == 0) :
+print(year)
+#else:
+       # year += 1
+        
+#print(year)
